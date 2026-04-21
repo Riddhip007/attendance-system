451393eea0659a91ee33017024d2845c67653abc
RIDDHIP VAGHELA <riddhipvaghela2005@gmail.com>
2026-04-22 05:16:30 +0530

    MarkAttendance.jsx

---
+++ MarkAttendance.jsx
@@ -0,0 +1,9 @@
+export default function MarkAttendance() {
+  return (
+    <div className="card">
+      <h3>🧠 AI Attendance</h3>
+      <button>Start Camera</button>
+      <button>Mark Attendance</button>
+    </div>
+  );
+}
