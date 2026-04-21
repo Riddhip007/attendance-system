c4390608fd7512cf3aaf6c0662cb94c1f6dfc78b
RIDDHIP VAGHELA <riddhipvaghela2005@gmail.com>
2026-04-22 05:17:04 +0530

    Add StudentList component to display students

---
+++ StudentList.jsx
@@ -0,0 +1,12 @@
+export default function StudentList() {
+  return (
+    <div className="card">
+      <h3>👨‍🎓 Students</h3>
+      <ul>
+        <li>Rahul</li>
+        <li>Priya</li>
+        <li>Amit</li>
+      </ul>
+    </div>
+  );
+}
