381f1ab4563934bcae85ab3e12323e9b9afdd950
RIDDHIP VAGHELA <riddhipvaghela2005@gmail.com>
2026-04-22 05:15:52 +0530

    Dashboard.jsx

---
+++ Dashboard.jsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <div className="card">
+      <h3>📊 Dashboard</h3>
+      <p>Total Students: 50</p>
+      <p>Present Today: 42</p>
+    </div>
+  );
+}
